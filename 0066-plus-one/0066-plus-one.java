7b898993a2787d809afa043f1610758ca2e44f15
Gourav Kumar <gk7410993@gmail.com>
2025-01-30 22:13:19 +0530

    Time: 0 ms (100%), Space: 42 MB (30.52%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,12 +2,14 @@ class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
         for(int i=n-1;i>=0;i--){
-            int d = digits[i];
-            if(d+1!=10){
-                d = d+1;
-                return d;
+            if(digits[i]+1!=10){
+                digits[i] = digits[i]+1;
+                return digits;
             }
-            
+            digits[i] = 0;
         }
+        int nd[] = new int[n+1];
+        nd[0] = 1;
+        return nd;
     }
 }
