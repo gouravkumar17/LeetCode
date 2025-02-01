8173ac7699df8dc7363b7bb5906dc7ffa9541000
Gourav Kumar <gk7410993@gmail.com>
2025-02-01 21:40:11 +0530

    Time: 0 ms (100%), Space: 43.6 MB (32.6%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
         int n = nums.length;
-        for(int i=0;i<n-1;i++){
-            int c1 = nums[i];
-            int c2 = nums[i+1];
+        for(int i=1;i<n;i++){
+            int c1 = nums[i-1];
+            int c2 = nums[i];
             if(c1%2==c2%2) return false;
         }
         return true;
