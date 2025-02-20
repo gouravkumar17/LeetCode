18b8c5a050ac8ee0dd950189e1f987b697522707
Gourav Kumar <gourav.kumar@lpu.in>
2025-02-20 11:11:47 +0530

    Time: 1 ms (100%), Space: 43.4 MB (91.98%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -5,10 +5,10 @@ class Solution {
         int arr[] = new int[128];
         int l = 0;
         for(int i=0;i<n;i++){
-            char c1 = s.charAt(i);
-            l = Math.max(arr[c1],l);
+            char c = s.charAt(i);
+            l = Math.max(arr[c],l);
             longest = Math.max(longest,i-l+1);
-            arr[c1] = i+1;
+            arr[c] = i+1;
         }
         return longest;
     }
