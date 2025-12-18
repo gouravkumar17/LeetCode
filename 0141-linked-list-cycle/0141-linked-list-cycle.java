59b244ec4bf4924c420b6b84040bd3e80a8727b7
Gourav Kumar <gourav.kumar@lpu.in>
2025-12-18 12:49:47 +0530

    Time: 0 ms (100%), Space: 46.9 MB (6.65%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,17 +11,13 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null) return false;
-        if(head.next == null) return false;
-        ListNode s = head;
-        ListNode f = head;
-        while(f != null){
-            if(s == null) return false;
-            s = s.next;
-            if(f.next == null) return false;
-            f = f.next.next;
-            if(f == s) return true;
-        }
-        return false;
+      ListNode f = head;
+      ListNode s = head;
+      while(f!=null && s!=null && s.next!=null){
+        f = f.next;
+        s = s.next.next;
+        if(f==s) return true;
+      }
+      return false;
     }
 }
