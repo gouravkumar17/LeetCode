d860981e6f6656910ab851bf147c2266b1cb04cf
Gourav Kumar <gourav.kumar@lpu.in>
2025-12-22 22:48:12 +0530

    Time: 2 ms (99.75%), Space: 52.5 MB (38.45%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,15 +1,48 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        // int n = nums.length;
+        // List<Integer> l = new ArrayList<>();
+        // Map<Integer,Integer> map = new HashMap<>();
+        // for(int num:nums){
+        //     map.put(num,map.getOrDefault(num,0)+1);
+        // }
+        // int s = n/3;
+        // for(var e:map.entrySet()){
+        //     if(e.getValue()>s) l.add(e.getKey());
+        // }
+        // return l;
+
+
         int n = nums.length;
-        List<Integer> l = new ArrayList<>();
-        Map<Integer,Integer> map = new HashMap<>();
+        int c1 = 0;
+        int c2 = 0;
+        int cnt1 = 0;
+        int cnt2 = 0;
         for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+            if(c1==num) cnt1++;
+            else if(c2==num) cnt2++;
+            else if(cnt1==0){
+                c1 = num;
+                cnt1 = 1;
+            }
+            else if(cnt2==0){
+                c2 = num;
+                cnt2 = 1;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
-        int s = n/3;
-        for(var e:map.entrySet()){
-            if(e.getValue()>s) l.add(e.getKey());
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int num:nums){
+            if(num==c1) cnt1++;
+            else if(num==c2) cnt2++;
         }
-        return l;
+        List<Integer> res = new ArrayList<>();
+        if(cnt1>n/3) res.add(c1);
+        if(cnt2>n/3) res.add(c2);
+        return res;
     }
 }
