3a8bdfe7afb0d7decb87ae185b37dbab0d1a0672
Gourav Kumar <gourav.kumar@lpu.in>
2025-12-25 23:43:51 +0530

    Time: 0 ms (100%), Space: 48.3 MB (7.33%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,19 +1,50 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
+    private int first(int nums[], int t){
         int n = nums.length;
-        int res[] = {-1,-1};
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                res[0] = i; 
-                break;
-            }
+        int idx = -1;
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]>=t) high = mid-1;
+            else low = mid+1;
+            if(nums[mid]==t) idx = mid;
         }
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]==target){
-                res[1] = i;
-                break;
-            }
+        return idx;
+    }
+    private int last(int nums[], int t){
+        int n = nums.length;
+        int idx = -1;
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]<=t) low = mid+1;
+            else high = mid-1;
+            if(nums[mid]==t) idx = mid;
         }
-        return res;
+        return idx;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        // int n = nums.length;
+        // int res[] = {-1,-1};
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==target){
+        //         res[0] = i; 
+        //         break;
+        //     }
+        // }
+        // for(int i=n-1;i>=0;i--){
+        //     if(nums[i]==target){
+        //         res[1] = i;
+        //         break;
+        //     }
+        // }
+        // return res;
+
+        int ans[] = new int[2];
+        ans[0] = first(nums,target);
+        ans[1] = last(nums,target);
+        return ans;
     }
 }
